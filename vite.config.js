52a69508f574779df4d6d6bc17b77266ac49b24d
andreaci <cortiandrea@hotmail.it>
2025-10-02 17:11:56 +0200

    test pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: './',
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
